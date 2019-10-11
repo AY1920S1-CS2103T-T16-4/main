e4d213d73ef84d7a3ca618c8372abb895efff300
Mo Zongran <mozongran@gmail.com>
2019-10-11 13:10:56 +0800

    Remove unused method

--- src/main/java/seedu/weme/commons/util/StringUtil.java
+++ src/main/java/seedu/weme/commons/util/StringUtil.java
@@ -39,30 +39,6 @@ public class StringUtil {
     }
 
     /**
-     * Returns true if the {@code sentence} contains the {@code word}.
-     *   Ignores case, and a full word match is not required.
-     *   <br>examples:<pre>
-     *       containsPartialWordIgnoreCase("ABc def", "abc") == true
-     *       containsPartialWordIgnoreCase("ABc def", "DEF") == true
-     *       containsPartialWordIgnoreCase("ABc def", "AB") == true //accepts partial match
-     *       </pre>
-     * @param sentence cannot be null
-     * @param word cannot be null, cannot be empty, must be a single word
-     */
-    public static boolean containsPartialWordIgnoreCase(String sentence, String word) {
-        requireNonNull(sentence);
-        requireNonNull(word);
-
-        String preppedWord = word.trim().toLowerCase();
-        checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
-
-        String preppedSentence = sentence.toLowerCase();
-
-        return preppedSentence.contains(preppedWord);
-    }
-
-    /**
      * Returns a detailed message of the t, including the stack trace.
      */
     public static String getDetails(Throwable t) {
